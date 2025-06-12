be313597ec920d343b5049de1c3fea0636c1ca1e
Dhruv <dhruvsahu7355@gmail.com>
2025-06-12 11:01:06 +0530

    Added level 1 practice programs in core-Java branch

---
+++ harry_Age.java
@@ -0,0 +1,8 @@
+class harry_Age{
+    public static void main(String args[]){
+        int birthYear = 2000;
+        int year = 2024;
+        int age = year-birthYear;
+        System.out.println(" Harry's age in 2024 is "+ age);
+    }
+}
---
+++ kilometersToMiles.java
@@ -0,0 +1,7 @@
+public class kilometersToMiles {
+    public static void main(String[] args) {
+        double distanceInKilometers = 10.8;
+        double distanceInMiles = 1.6*10.8;
+        System.out.println("The Distnace " + distanceInKilometers + "km in miles is "+ distanceInMiles);
+    }
+}

94a18a637114c081a35f61b90cabe089c7ac5d88
Dhruv <dhruvsahu7355@gmail.com>
2025-06-24 08:21:40 +0530

    Refactor

--- Control_flow/Level1/NatualSum.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class NatualSum {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        int sum=n*(n+1)/2; 
-        System.out.println("The sum of first " + n + " natural numbers is: " + sum);
-    }
-}
--- Day1_ProgrammingElements/Level1/course_discount.java
+++
@@ -1,9 +0,0 @@
-public class course_discount {
-    public static void main(String[] args) {
-        int fee = 125000;
-        int discountPercent = 10;
-        int discount = (fee *10)/100;
-        int discountedAmount = fee - discount;
-        System.out.println("The discount amount is INR "+discount+" and the final discounted fee is INR "+discountedAmount);
-    }
-}
